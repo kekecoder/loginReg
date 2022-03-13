86345cb2f004ee23ac1e777d16f0bfba2cfd9101
kekecoder <abimbola.felix@gmail.com>
2022-03-13 15:21:31 +0100

    added necessary requirement to database config

--- partials/dbconfig.php
+++ partials/dbconfig.php
@@ -1,11 +1,27 @@
 <?php
-
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1; port=3306; dbname=loginreg; charset=utf8;', 'root', 'jerusalem1991');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    //print "connected successfully";
-} catch (Error $e) {
-    print "An Exception has occured " . $e->getMessage();
-    print "Something went wrong, please try again later.";
-}
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+  
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+        $pdo = new PDO("mysql:host=$hostname; port=3306; dbname=$database; ", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+      //print "connected successfully";
+      $sql = "CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
+    created_at DATETIME
+    ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+    $pdo->exec($sql);
+    print "table created successfully";
+  } catch (Error $e) {
+      //print "An Exception has occured " . $e->getMessage();
+      print "Something went wrong, please try again later.";
+  }
